b0979796634f120f9d74ff50258287375b6c9d51
chinmayi-r <47920064+chinmayi-r@users.noreply.github.com>
2019-08-21 22:38:11 +0530

    Update PIR-LDR.ino

--- PIR-LDR.ino
+++ PIR-LDR.ino
@@ -1,4 +1,5 @@
-const int in1 = 9;
+const int in1 = 9;//Bulb relay
+const int in2 = 11;//Fan relay
 const int PIR_1 = 10;
 const int PIR_2 = 5;
 const int LDR = A0;
@@ -9,6 +10,7 @@ float LDRval = 0;
 
 void setup() {
   pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
   pinMode(PIR_1, INPUT);
   pinMode(PIR_2, INPUT);
   pinMode(led, OUTPUT);
@@ -17,6 +19,7 @@ void setup() {
   digitalWrite(led,LOW);
   digitalWrite(led1,LOW);
   digitalWrite(in1,LOW);
+  digitalWrite(in2,LOW);
 
   while(millis() < 5000){
     digitalWrite(led,HIGH);
@@ -33,42 +36,59 @@ void setup() {
 
 void loop() {
   LDRval = analogRead(LDR);
-  digitalWrite(led,LOW);
-  digitalWrite(led1,LOW);
-  digitalWrite(in1,LOW);
+
   if(digitalRead(PIR_1)==HIGH||digitalRead(PIR_2)==HIGH){
+   digitalWrite(led,LOW);
+   digitalWrite(led1,LOW);
+   digitalWrite(in1,LOW);
    t=millis();
    while(millis()<(t+5000)){
     if (LDRval<120){
     digitalWrite(led,HIGH);
     digitalWrite(led1,HIGH);
     digitalWrite(in1,HIGH);
+    digitalWrite(in2,HIGH);
     }
     else{   
     digitalWrite(led,LOW);
     digitalWrite(led1,LOW);
-    digitalWrite(in1,HIGH);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
     }
     delay(2300);
-    digitalWrite(led,LOW);
-    digitalWrite(led1,LOW);
     if ((digitalRead(PIR_1)==HIGH||digitalRead(PIR_2)==HIGH)) {
+       digitalWrite(led,LOW);
+       digitalWrite(led1,LOW);
+       digitalWrite(in1,LOW);
        if (LDRval<120){
          digitalWrite(led,HIGH);
          digitalWrite(led1,HIGH);
          t=millis();
        }
-     }
+       else{
+        digitalWrite(in1,LOW);
+        digitalWrite(in2,LOW);
+        if (LDRval<120){
+         digitalWrite(led1,HIGH);    
+         }
+        else{    
+         digitalWrite(led1,LOW);
+         }
+        }
+      }
+    }
    }
-  }
   else{
+    digitalWrite(led,LOW);
+    digitalWrite(led1,LOW);
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
     if (LDRval<120){
          digitalWrite(led1,HIGH);
-  }
-  else{    
-    digitalWrite(led1,LOW);
-  }
-
-    
-  }
+         digitalWrite(in1,LOW);
+    }
+    else{    
+     digitalWrite(led1,LOW);
+    }
+   }
 }
